322a0c261fcf6a46ba83be90d13f9bd4b3b3d0cd
= <rtyenriques@gmail.com>
2021-11-23 15:44:00 -0800

    commit tree 8a89530d5d3c0cbb683b84d2ff5384448326a497

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//finds the element by id
+const element = document.getElementById("main");  
+// deletes the element
+element.remove();
+
+//has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+
+// has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+newHeader.innerHTML = "YOUR - NAME is the champion"; 
